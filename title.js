1043609f84ff34a5ee143b291fa7526a18d258c2
bubatz <71530056+cynhook@users.noreply.github.com>
2022-04-28 15:39:13 -0400

    Add files via upload

---
+++ title.js
@@ -0,0 +1,3 @@
+var i=0;setInterval(function(){var titles=[" cynical "," is  "," better "," LOL "," fortnite "," danced "," on "," niggqa "," balls "]
+if(i===titles.length){i=0;}
+document.title=titles[i];i++;},900);
