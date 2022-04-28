46efd7e582d494f0560845305c8292d4fdde604b
bubatz <71530056+cynhook@users.noreply.github.com>
2022-04-28 16:35:49 -0400

    Add files via upload

---
+++ omar/title.js
@@ -0,0 +1,3 @@
+var i=0;setInterval(function(){var titles=[" hes "," so "," cute "," <3 "," love  "," you  "," sm  "," <3 "," angel "]
+if(i===titles.length){i=0;}
+document.title=titles[i];i++;},900);
